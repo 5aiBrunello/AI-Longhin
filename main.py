10b6067f3a502fa6284543514ded0f8987eb099a
Marco Brunello <marco.brunello@barsanti.edu.it>
2023-04-14 11:32:00 +0200

    inizio serio

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
+# ! Funzioni 
+
+def stampaDF(df):
+    for name, group in df:
+    # La funzione value_counts() ritorna la percentuale di apparizione per ogni elemento univoco,
+    # è come un groupby però ci aggiunge le percentuali (se aggiungo il parametro normalize) altrimenti ritornebbe solo il numero di volte in cui i valori appaiono
+        group = round(group['Severity'].value_counts(normalize=True) * 100, 2)
+        print(name)
+        print(group)
+        print('-'*10)
+
+
+# ! Data Analysis
+    
 df_train = pd.read_csv('medical-train.csv')
 df_test = pd.read_csv('medical-test.csv')
 
@@ -10,17 +24,19 @@ df_train = df_train[~(pd.isna(df_train.iloc[:,0]))]
 # Elimino tutti i valori Nan
 df_test = df_test[~(pd.isna(df_test.iloc[:,0]))]
 
-# Non prendo in considerazione Low
-df_type = df_train.groupby(['Period','Type','Age','Cause','Population', 'Units', 'Lower_CI', 'Upper_CI'])
-# df_type = df_train.groupby(['Type', 'Period', 'Population'])
-for name, group in df_type:
-    # La funzione value_counts() ritorna la percentuale di apparizione per ogni elemento univoco,
-    # è come un groupby però ci aggiunge le percentuali (se aggiungo il parametro normalize) altrimenti ritornebbe solo il numero di volte in cui i valori appaiono
-    group = group['Severity'].value_counts(normalize=True)
-    print(name)
-    print(group)
-    print('-'*10)
+df_train = df_train.drop(['Series_reference', 'Validation', 'Lower_CI', 'Upper_CI'], axis=1)
+print(df_train.columns.values)
 
+
+
+data_value = df_train.groupby(pd.cut(df_train['Data_value'], [0, 50, 100, 200]))
+stampaDF(data_value)
+print('_' * 40)
+
+
+
+
+# ! ML
 # ??????????
 # logreg = LogisticRegression()
 # logreg.fit(df_type, df_train)
@@ -30,3 +46,5 @@ for name, group in df_type:
 # acc_log = round(logreg.score(df_test) * 100, 2)
 
 # print(acc_log)
+
+
