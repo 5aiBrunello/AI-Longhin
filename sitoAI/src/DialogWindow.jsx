37561d4c65db7c33f0bde52800feef16b8aab084
5aiBrunello <marco.brunello@barsanti.edu.it>
2023-04-30 22:55:00 +0200

    Fatto rendering delle parole come chatGPT

---
+++ sitoAI/src/DialogWindow.jsx
@@ -0,0 +1,35 @@
+import avatar from "./assets/images/avatar.svg"
+import ai from "./assets/images/AI.svg"
+import { useTypewriter, Cursor } from "react-simple-typewriter"
+
+function DialogWindow({ whoisTalking, text, setQuestionOn = () => {} }) {
+  const [typedText, typeSettings] = useTypewriter({
+    words: [text],
+    typeSpeed: 70,
+    loop: 1,
+    onLoopDone: () => {
+      setQuestionOn(true)
+    },
+  })
+
+  return (
+    <section className="dialog">
+      {/* Renderizzo immagine in base a who is talking */}
+      {whoisTalking === "User" ? (
+        <img src={avatar} alt="" className="icon" />
+      ) : (
+        <img src={ai} alt="" className="icon" id="ai" />
+      )}
+
+      <div className="display-message">
+        <span>{typedText}</span>
+        <span>
+          {!typeSettings.isDone && (
+            <Cursor cursorStyle="_" cursorBlinking={true} />
+          )}
+        </span>
+      </div>
+    </section>
+  )
+}
+export default DialogWindow
--- sitoAI/src/ResponseContainer.jsx
+++ sitoAI/src/ResponseContainer.jsx
@@ -1,41 +1,26 @@
-import avatar from "./assets/images/avatar.svg"
-import ai from "./assets/images/AI.svg"
-
 import "./DialogWindow.css"
+import { useState } from "react"
+import Chat from "./DialogWindow"
 
 function ResponseContainer() {
+  const [isQuestionDone, setIsQuestionDone] = useState(false)
   return (
     <section className="response">
-      <DialogWindow
+      <Chat
         whoisTalking={"User"}
         text={
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia repellat provident deserunt accusantium eos adipisci neque obcaecati laudantium, magnam dignissimos at reprehenderit distinctio cum nemo cumque nobis repellendus ipsum? Possimus."
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, nam. Voluptas vitae sint accusantium perferendis? Consequatur at necessitatibus ad nulla id eum ex dolore, cupiditate voluptatibus doloribus! Officiis, modi dignissimos Accusamus, minus ipsum repellendus quod asperiores quis maxime libero velit obcaecati distinctio commodi officia porro delectus voluptates quibusdam quos. A illo in vero modi ipsam ad! Nobis ad voluptatem rem!"
         }
+        setQuestionOn={setIsQuestionDone}
       />
-
-      <DialogWindow
-        text={
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia repellat provident deserunt accusantium eos adipisci neque obcaecati laudantium, magnam dignissimos at reprehenderit distinctio cum nemo cumque nobis repellendus ipsum? Possimus."
-        }
-      />
+      {isQuestionDone && (
+        <Chat
+          text={
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, nam. Voluptas vitae sint accusantium perferendis? Consequatur at necessitatibus ad nulla id eum ex dolore, cupiditate voluptatibus doloribus! Officiis, modi dignissimos.Accusamus, minus ipsum repellendus quod asperiores quis maxime libero velit obcaecati distinctio commodi officia porro delectus voluptates quibusdam quos. A illo in vero modi ipsam ad! Nobis ad voluptatem rem!"
+          }
+        />
+      )}
     </section>
   )
 }
 export default ResponseContainer
-
-function DialogWindow({ whoisTalking, text }) {
-  return (
-    <section className="dialog">
-      {/* Renderizzo immagine in base a who is talking */}
-      {whoisTalking === "User" ? (
-        <img src={avatar} alt="" className="icon" />
-      ) : (
-        <img src={ai} alt="" className="icon" id="ai" />
-      )}
-
-      <div className="display-message">
-        <p>{text}</p>
-      </div>
-    </section>
-  )
-}
